0269d70a4f4f87aca2fa0a97142ed67ca43ac3d4
Issa Soumare <isoummare@atixnet.fr>
2016-11-01 23:01:05 +0100

    part1

---
+++ partie1-arborescence/public/index.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+$name = "blog";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+ <meta charset="UTF-8">
+ <title>Blog</title>
+</head>
+<body>
+<h1>Hello <?php print $name; ?>!</h1>
+</body>
+</html>
